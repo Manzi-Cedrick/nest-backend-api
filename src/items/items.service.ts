9bd359bae94def945075f21db86dd0a1986829ab
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-10-21 21:55:51 +0200

    Item Services Initial Assets

---
+++ src/items/items.service.ts
@@ -0,0 +1,30 @@
+/* eslint-disable prettier/prettier */
+import { Injectable } from '@nestjs/common';
+import { ItemsInterface } from './interfaces/item.interface';
+
+@Injectable()
+export class ItemsService {
+    private readonly items:ItemsInterface[]=[
+        {
+            id : "sdjsfa2234",
+            name : "item1",
+            quantity:23402,
+            description:"Item One"
+        },
+        {
+            id : "asdf23901ksadf",
+            name : "item2",
+            quantity:23402,
+            description:"Item Two"
+        },
+        {
+            id : "asd023-ldsaf;lkasdf",
+            name : "item3",
+            quantity:23402,
+            description:"Item Three"
+        },
+    ];
+    findAll():ItemsInterface[]{
+        return this.items;
+    }
+}
