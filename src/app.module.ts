beb4b03bca652afb72ea59000c59aee128e12602
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-10-22 10:30:23 +0200

    fix : item service and controller import

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import config from './config/keys';
-import { ItemsController } from './items/items.controller';
 import { ItemsModule } from './items/items.module';
-import { ItemsService } from './items/items.service';
 
 @Module({
   imports: [ItemsModule,MongooseModule.forRoot(config.mongoURL)],
-  controllers: [AppController, ItemsController],
-  providers: [AppService, ItemsService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
