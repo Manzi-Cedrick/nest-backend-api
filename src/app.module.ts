14e50abdd681b81d7a19a17f96907f2025cfb492
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-10-22 10:04:29 +0200

    Item Module referencing

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import config from './config/keys';
 import { ItemsController } from './items/items.controller';
+import { ItemsModule } from './items/items.module';
 import { ItemsService } from './items/items.service';
 
 @Module({
-  imports: [],
+  imports: [ItemsModule,MongooseModule.forRoot(config.mongoURL)],
   controllers: [AppController, ItemsController],
   providers: [AppService, ItemsService],
 })
