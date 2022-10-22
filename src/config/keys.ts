6868efb84f6c0665a2d66777b7e274c1185f84cf
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-10-22 10:04:50 +0200

    mongourl environment addition

---
+++ src/config/keys.ts
@@ -0,0 +1,4 @@
+/* eslint-disable prettier/prettier */
+export default {
+    mongoURL : 'mongodb+srv://Manzi-Cedrick:kabanda2005@cluster0.mstpo.mongodb.net/nestbackend?retryWrites=true&w=majority'
+}
