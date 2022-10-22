c1cd25832e5e6c5f4913035619d5144736f8f0ac
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-10-22 10:32:10 +0200

    Item schema config

---
+++ src/items/schema/item.schema.ts
@@ -0,0 +1,7 @@
+/* eslint-disable prettier/prettier */
+import * as mongoose from 'mongoose';
+export const ItemSchema = new mongoose.Schema({
+    name: String,
+    quantity: Number,
+    description:String,
+});
