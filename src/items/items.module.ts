4543ac311d5e27e2d3258042e359bfc30af67571
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-10-22 10:31:27 +0200

    add: Mongoose Package addition

--- src/items/items.module.ts
+++ src/items/items.module.ts
@@ -2,9 +2,11 @@
 import { Module } from "@nestjs/common";
 import { ItemsController } from "./items.controller";
 import { ItemsService } from "./items.service";
+import { MongooseModule } from '@nestjs/mongoose';
+import { ItemSchema } from "./schema/item.schema";
 
 @Module({
-    imports: [],
+    imports: [MongooseModule.forFeature([{ name: 'Item',schema:ItemSchema}])],
     controllers:[ItemsController],
     providers:[ItemsService]
 })
