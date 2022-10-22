bac4ae6ee42dc5e3c94510b1f267e29805b9ed7b
Manzi-Cedrick <cedrickmanzii0@gmail.com>
2022-10-22 10:44:38 +0200

    crud services addition

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -14,5 +14,14 @@ export class ItemsService {
     async findOne(id:string):Promise<ItemsInterface>{
         return await this.itemModelData.findById({_id:id});
     }
-
+    async createOne(item:ItemsInterface):Promise<ItemsInterface>{
+        const newItem = new this.itemModelData(item);
+        return await newItem.save();
+    }
+    async deleteOne(id:string):Promise<void>{
+        return await this.itemModelData.findByIdAndRemove(id);
+    }
+    async updateOne(id:string, item:ItemsInterface):Promise<ItemsInterface>{
+        return await this.itemModelData.findByIdAndUpdate(id,item,{new:true});
+    }
 }
